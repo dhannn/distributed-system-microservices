e923820bdda3b52dd646c650e0cba8a2c49b4534
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2024-04-05 17:28:16 +0800

    Integrate new changes to the listener

--- replication-manager/listener.py
+++ replication-manager/listener.py
@@ -1,105 +1,22 @@
 import socket
-import json
-import os
-import math
 import sys
 
-# assuming line by line reading of the file
-# emitter sends "command" insert or update, send using JSON 
-# send region
-# if update also send id and status
-
-def luzon_update(id, status):
-    success = False
-    luzon_host = '10.2.0.100'
-    luzon_port = 3306  
-    luzon_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        luzon_socket.connect((luzon_host, luzon_port))
-        update_query = "UPDATE Appointments SET status = '{}' WHERE id = {}".format(status, id)
-        luzon_socket.sendall(update_query.encode('utf-8'))
-        print("Successful replication to luzon database")
-        success = True
-    except socket.error as e:
-        print(str(e))
-        print("Error connecting to replica database")
-    finally:
-        luzon_socket.close()
-
-    if success:
-        return "ACK"
-    else:
-        return "NACK"
-
-def visMin_update(id, status):
-    success = False
-    visMin_host = '10.2.0.100'
-    visMin_port = 3306  
-    visMin_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        visMin_socket.connect((visMin_host, visMin_port))
-        update_query = "UPDATE Appointments SET status = '{}' WHERE id = {}".format(status, id)
-        visMin_socket.sendall(update_query.encode('utf-8'))
-        print("Successful replication to luzon database")
-        success = True
-    except socket.error as e:
-        print(str(e))
-        print("Error connecting to replica database")
-    finally:
-        visMin_socket.close()
-
-    if success:
-        return "ACK"
-    else:
-        return "NACK"
-
-def luzon_insert():
-    success = False
-    luzon_host = '10.2.0.101'
-    luzon_port = 3306  
-    luzon_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        luzon_socket.connect((luzon_host, luzon_port))
-        insert_query = "INSERT INTO Appointments (region) VALUES (Luzon)"
-        luzon_socket.sendall(insert_query.encode('utf-8'))
-        print("Successful replication to luzon database")
-        success = True
-    except socket.error as e:
-        print(str(e))
-        print("Error connecting to replica database")
-    finally:
-        luzon_socket.close()
-
-    if success:
-        return "ACK"
-    else:
-        return "NACK"
-    
-def visMin_insert():
-    success = False
-    visMin_host = '10.2.0.100'
-    visMin_port = 3306  
-    visMin_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        visMin_socket.connect((visMin_host, visMin_port))
-        insert_query = "INSERT INTO Appointments (region) VALUES (Luzon)"
-        visMin_socket.sendall(insert_query.encode('utf-8'))
-        print("Successful replication to luzon database")
-        success = True
-    except socket.error as e:
-        print(str(e))
-        print("Error connecting to replica database")
-    finally:
-        visMin_socket.close()
-
-    if success:
-        return "ACK"
-    else:
-        return "NACK"
+import mysql.connector
+from transactions import *
 
 def listener(host, port):
-    res = {"res":"nan"}
     listener_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transactions = Transactions()
+    parser = LogParser()
+    conn = Connection()
+
+    _dict = {
+        'START': transactions.start,
+        'COMMIT': transactions.end,
+        'ABORT': transactions.end,
+        'INSERT': transactions.add_operation,
+        'MODIFY': transactions.add_operation,
+    }
 
     # bind host and port
     try:
@@ -108,33 +25,19 @@ def listener(host, port):
         print(str(e))
         print("Error binding to host and port")
         sys.exit()
+    
     try:
         while True:
-            data, addr = socket.recvfrom(1024)	        
-        
-            try: 
-                data = json.loads(data.decode('utf-8'))
-            except:
-                print("Invalid data received")
+            data, addr = listener_socket.recvfrom(1024)
+            ret = parser.parse(data)
 
-            cmd = data['cmd']
-            region = data['region']
+            try:
+                query = _dict(ret[1])(*ret)
+                if query is not None:
+                    conn.execute_query(query)
+            except Exception as e:
+                listener_socket.send(f'NACK {e}'.encode('utf-8'))
 
-            if cmd == 'update':
-                id = data['id']
-                status = data['status']
-                if region == 'luzon':
-                    res["res"] = luzon_update(id, status)
-                elif region == 'visMin':
-                    res["res"] = visMin_update(id, status)
-            elif cmd == 'insert':
-                if region == 'luzon':
-                    res["res"] = luzon_insert()
-                elif region == 'visMin':
-                    res["res"] = visMin_insert()
-                
-            jmsg = json.dumps(res).encode('utf-8')
-            socket.sendto(jmsg, addr)
 
     except KeyboardInterrupt:
         print("Program terminated.")
--- replication-manager/transactions.py
+++ replication-manager/transactions.py
@@ -1,29 +1,5 @@
 import mysql.connector
 import os
-transactions = {
-
-}
-
-# Once it received LSN + START, it will put the lsn key
-# to transactions
-
-transactions['001'] = []
-
-# For every succeeding logs, we append to the list
-transactions['001'] = [{
-    'operation': 'INSERT',
-    'id': '1',
-    'args': ['03-29-2024', 'Luzon', 'Queued', '0']
-    }]
-
-# Once it received LSN + COMMIT/ABORT, 
-# do the operation or not
-
-""" 
-    Saves & parses the transaction log from other emitters
-"""
-# raw transaction log input from the emitter
-# call this 
 
 class Transactions:
     transactions = {}
@@ -31,21 +7,21 @@ class Transactions:
     def start(self, lsn):
         self.transactions[lsn] = []
 
-    def add_operation(self, lsn, operation, id, args):
+    def add_operation(self, lsn, operation, args):
         db_operation = {
             'operation': operation,
-            'id': id,
-            'args': args
+            'id': args[0],
+            'args': args[1:]
         }
 
         self.transactions[lsn].append(db_operation)
 
     def end(self, lsn, status):
-        def convert_to_query(operation, id, args):
+        def convert_to_query(operation, args):
             if operation == 'INSERT':
-                return f"INSERT INTO Appointments VALUES ({id}, {", ".join(args) });"
+                return f"INSERT INTO Appointments VALUES ({args[0]}, {', '.join(args) });"
             elif operation == 'MODIFY':
-                return f"UPDATE Appointments SET {args[0]} = {args[2]} WHERE id = {id};"
+                return f"UPDATE Appointments SET {args[1]} = {args[3]} WHERE id = {args[0]};"
             else:
                 raise 'Unsupported operation'
 
@@ -60,34 +36,27 @@ class Transactions:
             return query
 
 class LogParser:
-    def __init__(self, transactions: Transactions) -> None:
-        self.transactions = transactions
+    @staticmethod
+    def parse(log):
+        lsn, operation, *args = log.split(' ')
 
-    def parse(self, log):
-        lsn, operation, args = log.split(' ')
-
-        if operation == 'START':
-            self.transactions.start(lsn)
-        elif operation == 'COMMIT' or operation == 'ABORT':
-            self.transactions.end(lsn, operation)
-        elif operation == 'INSERT' or operation == 'MODIFY':
-            self.transactions.add_operation(lsn, operation, args[0], args[1:])
-        else: 
-            raise Exception('Unsupported operation')
+        return lsn, operation, args
 
 class Connection:
 
     db_connection = mysql.connector.MySQLConnection
 
-    # Initialize database connection
     def __init__(self):
-        self.db_connection = mysql.connector.connect(
-            user=os.environ['DB_SERVER_USER'],
-            password=os.environ['DB_SERVER_PASS'],
-            host=os.environ['DB_SERVER_HOST'],
-            port=os.environ['DB_SERVER_PORT'],
-            database='SeriousMD'
-        )
+        try:
+            self.db_connection = mysql.connector.connect(
+                user=os.environ['DB_SERVER_USER'],
+                password=os.environ['DB_SERVER_PASS'],
+                host=os.environ['DB_SERVER_HOST'],
+                port=os.environ['DB_SERVER_PORT'],
+                database='SeriousMD'
+            )
+        except Exception as e:
+            print(e)
     
     def execute_query(self, query):
         is_success = True
