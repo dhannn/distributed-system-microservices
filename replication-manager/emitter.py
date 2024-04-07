872265f5e5cdb2e7d21e03877bf76f70d87be6da
xian <xianirabernardo@gmail.com>
2024-04-07 17:00:26 +0800

    emitter commit

---
+++ replication-manager/emitter.py
@@ -0,0 +1,44 @@
+import time
+import socket
+import os
+import hashlib
+
+def file_checksum(filename):
+    with open(filename, 'rb') as f:
+        bytes = f.read()
+        readable_hash = hashlib.md5(bytes).hexdigest()
+    return readable_hash
+
+def emitter(nodes, files_to_monitor):
+    # create sockets for each node
+    sockets = [socket.socket(socket.AF_INET, socket.SOCK_STREAM) for _ in nodes]
+
+    # connect to nodes
+    for sock, (host, port) in zip(sockets, nodes):
+        sock.connect((host, port))
+
+    # get initial file hashes
+    last_known_hashes = {filename: file_checksum(filename) for filename in files_to_monitor}
+
+    # monitor file changes and replicate file data to other nodes
+    while True:
+        for filename in files_to_monitor:
+            current_hash = file_checksum(filename)
+            if current_hash != last_known_hashes[filename]:
+                last_known_hashes[filename] = current_hash
+                with open(filename, 'r') as file:
+                    data = file.read()
+
+                for sock in sockets:
+                    sock.sendall(data.encode('utf-8'))
+
+                    # ACK/NACK part
+                    response = sock.recv(1024).decode('utf-8')
+                    if response.startswith('ACK'):
+                        print(f"Replication of {filename} successful")
+                    elif response.startswith('NACK'):
+                        print(f"Replication of {filename} unsuccessful, retrying...")
+                        time.sleep(5)
+                    else:
+                        print("Unknown response:", response)
+        time.sleep(1)
