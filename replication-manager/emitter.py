f6a9702b455745f1c7b6480734e7440f7b5e53e9
xian <xianirabernardo@gmail.com>
2024-04-07 19:10:34 +0800

    updated emitter

--- replication-manager/emitter.py
+++ replication-manager/emitter.py
@@ -8,46 +8,48 @@ def file_checksum(filename):
         readable_hash = hashlib.md5(bytes).hexdigest()
     return readable_hash
 
-def emitter(nodes, files_to_monitor):
+def emitter(nodes, filename_to_monitor):
     # create sockets for each node
     sockets = [socket.socket(socket.AF_INET, socket.SOCK_STREAM) for _ in nodes]
 
     # connect to nodes
     for sock, (host, port) in zip(sockets, nodes):
         sock.connect((host, port))
+        
+    current_socket_index = 0
 
-    # get initial file hashes
-    last_known_hashes = {filename: file_checksum(filename) for filename in files_to_monitor}
+    #get initial hash
+    last_known_hash = file_checksum(filename_to_monitor)
 
-    # monitor file changes and replicate file data to other nodes
+    # monitor file changes and replicate file data to the last node
     while True:
-        for filename in files_to_monitor:
-            current_hash = file_checksum(filename)
-            if current_hash != last_known_hashes[filename]:
-                last_known_hashes[filename] = current_hash
-                with open(filename, 'r') as file:
-                    data = file.read()
+        current_hash = file_checksum(filename_to_monitor)
+        if current_hash != last_known_hash:
+            last_known_hash = current_hash
+            with open(filename_to_monitor, 'r') as file:
+                data = file.read()
 
-                for sock in sockets:
-                    sock.sendall(data.encode('utf-8'))
+            sock = sockets[current_socket_index]
+            sock.sendall(data.encode('utf-8'))
+            current_socket_index = (current_socket_index + 1) % len(sockets)
 
-                    # ACK/NACK part
-                    response = sock.recv(1024).decode('utf-8')
-                    backoff_time = 1
-                    max_backoff_time = 60
+            # ACK/NACK part
+            response = sock.recv(1024).decode('utf-8')
+            backoff_time = 1
+            max_backoff_time = 60
 
-                    while response.startswith('NACK') and backoff_time <= max_backoff_time:
-                        print(f"Replication of {filename} unsuccessful, retrying in {backoff_time} seconds...")
-                        time.sleep(backoff_time)
-                        sock.sendall(data.encode('utf-8'))
-                        response = sock.recv(1024).decode('utf-8')
-                        backoff_time *= 2
+            while response.startswith('NACK') and backoff_time <= max_backoff_time:
+                print(f"Replication of {filename_to_monitor} unsuccessful, retrying in {backoff_time} seconds...")
+                time.sleep(backoff_time)
+                sock.sendall(data.encode('utf-8'))
+                response = sock.recv(1024).decode('utf-8')
+                backoff_time *= 2
 
-                    if response.startswith('ACK'):
-                        print(f"Replication of {filename} successful")
-                    elif response.startswith('NACK'):
-                        print(f"Replication of {filename} unsuccessful, giving up after {backoff_time//2} seconds")
-                    else:
-                        print("Unknown response:", response)
+            if response.startswith('ACK'):
+                print(f"Replication of {filename_to_monitor} successful")
+            elif response.startswith('NACK'):
+                print(f"Replication of {filename_to_monitor} unsuccessful, giving up after {backoff_time//2} seconds")
+            else:
+                print("Unknown response:", response)
                         
         time.sleep(1)
