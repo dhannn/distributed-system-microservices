7f481517a903643b6049edb93b20b4b55e16493a
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2024-04-10 13:07:35 +0800

    Add script for viewing appointments

--- src/js/searchButton.js
+++ src/js/searchButton.js
@@ -4,8 +4,22 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   const searchButton = document.getElementById("search-button");
-  searchButton.addEventListener("click", function () {
+  searchButton.addEventListener("click", async function () {
     // Add search functionality here
+    const apptId = document.getElementsByClassName('search-field')[0].textContent;
+
+    try {
+    
+      const response = await fetch(`appts/id?=${ apptId }`, {
+        method: 'GET'
+      });
+      
+      console.log(response);
+    
+    } catch (error) {
+      console.error(error);  
+    }
+
 
     // Redirect to resulting table page
 
