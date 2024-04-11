82346f8314faf22c984941e76b95242400168f1a
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2024-04-11 11:11:39 +0800

    Modify generate report button

--- src/js/genReportButton.js
+++ src/js/genReportButton.js
@@ -4,9 +4,16 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     const createReportBtn = document.querySelector(".create-report-btn");
-    createReportBtn.addEventListener("click", function() {
+    createReportBtn.addEventListener("click", async function() {
       // Add generate report functionality here
-      
+      try {
+        const response = await fetch('report/');
+        if (response.ok) {
+          window.location.href = `pages/table.html?apptId=${apptId}`;
+        }
+      } catch (error) {
+        console.error(error);
+      }
         
       alert("Generate Report Clicked");
     });
