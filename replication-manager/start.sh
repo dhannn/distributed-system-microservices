990be1910c4d90a240d6b3120d7e58a9d6adea75
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2024-04-13 15:24:14 +0800

    [Post-submission] Send necessary logs

---
+++ replication-manager/start.sh
@@ -0,0 +1,5 @@
+
+pkill -9 python3
+nohup python3 -u emitter.py > /root/log_files/replication_emitter.log & disown
+
+nohup python3 -u listener.py > /root/log_files/replication_listener.log & disown
