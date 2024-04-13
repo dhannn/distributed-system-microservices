6e3a6b6090aa3ae22d770ca4fe6937850accd752
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2024-04-13 16:42:47 +0800

    [Post-submission] Send necessary logs

--- cleandb.sh
+++ cleandb.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
-mysql --execute='USE SeriousMD; source sql/init.sql'
-mysql --execute='USE SeriousMD; source sql/trigger.sql'
+mysql --execute='USE SeriousMD' < source sql/init.sql
+mysql --execute='USE SeriousMD' < source sql/trigger.sql
